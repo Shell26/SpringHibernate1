e4b52be6804cb452c5b7564d5008272866407d08
Shell26 <M16VSAK47vsgit>
2019-11-21 12:31:32 +0400

    Add Thymeleaf page

--- src/main/java/shell/controlers/UsersController.java
+++ src/main/java/shell/controlers/UsersController.java
@@ -42,43 +42,6 @@ public class UsersController {
         this.userService = userService;
     }
 
-    @RequestMapping(path = "/admin", method = RequestMethod.GET)
-    public ModelAndView getAllUsers() {
-        List<User> users = userService.findAllUser();
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("mainPage");
-        modelAndView.addObject("usersFromServer", users);
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/admin/delete/{*}", method = RequestMethod.POST)
-    public String deleteUser(@PathVariable("*") Long userId) {
-        userService.deleteById(userId);
-        return "redirect:/admin";
-    }
-
-    @RequestMapping(value = "/admin/edit/{*}", method = RequestMethod.GET)
-    public ModelAndView editPage(@PathVariable("*") long id) { //@PathVariable указывает на то, что данный параметр (int id) получается из адресной строки
-        User user = userService.findOneById(id);
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("editPage");
-        user.setId(id);
-        modelAndView.addObject("user", user);
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/admin/edit", method = RequestMethod.POST)
-    public ModelAndView editUser(@ModelAttribute("user") User user) {
-        User userFromDB = userService.findOneById(user.getId());
-        userFromDB.setLogin(user.getLogin());
-        userFromDB.setName(user.getName());
-        userFromDB.setPassword(user.getPassword());
-        userRepository.save(userFromDB);
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/admin"); //означает, что после выполнения данного метода мы будем перенаправлены на адрес "/"
-        return modelAndView;
-    }
-
     @GetMapping("/login")
     public String getLoginPage (Authentication authentication, ModelMap model, HttpServletRequest request) {
         if (authentication != null) {
@@ -104,35 +67,5 @@ public class UsersController {
             return "redirect: /user";
         }
         return "login";
-    }
-
-    @RequestMapping(path = "/admin/save", method = RequestMethod.POST)
-    public String saveUsers(User user) {
-        user.setRoles(roleService.getRoleByName("USER"));
-        userService.addUser(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/signUp")
-    public String getSignUpPage() {
-        return "signUp";
-    }
-
-    @PostMapping("/signUp")
-    public String signUp (User user, String role) {
-        if (role.equals("USER")) {
-            user.setRoles(roleService.getRoleByName(role));
-        } else {
-            user.setRoles(roleService.getRoleByName("ADMIN"));
-        }
-        userRepository.save(user);
-        return "redirect:/login";
-    }
-
-    @RequestMapping(path = "/admin/save", method = RequestMethod.GET)
-    public ModelAndView save() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("save");
-        return modelAndView;
     }
 }
